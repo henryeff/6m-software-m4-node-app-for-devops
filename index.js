60b2cf3126ab7d325288c4442ff447ea7f7d9fe2
Hendry Eff <hendryeff@live.com>
2024-04-17 21:18:45 +0800

    added new endpoints

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ app.get("/introduce", (req, res) => {
   res.send("hello I'm Hendry");
 });
 
+app.get("/home", (req, res) => {
+  res.send("this is the home page");
+});
+
+app.post("/user", (req, res) => {
+  const { name, email, password } = req.body;
+  res.send(`Name: ${name}, email: ${email}, password: ${password}`);
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Listening to port ${process.env.PORT}`);
 });
